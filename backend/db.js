85c674a3faf26f84ca2b2417bed93ceedb8840b5
gtengine <prierkt@gmail.com>
2023-02-23 17:47:57 +0900

    create Dockerrun file

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const myspl = require("mysql");
 const pool = myspl.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "devops",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 exports.pool = pool;
